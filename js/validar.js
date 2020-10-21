5e9700475e7384c26ffaa959c47fbaa9175979b1
igorodriguez <57678396+igorodriguez@users.noreply.github.com>
2020-10-21 10:29:20 +0200

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function (event){
+    returnDatos();
+    correo();
+    
+})
+
+function returnDatos(){
+    if(correo() && clave()){
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+function correo(){
+    if(window.document.forms.encuesta.correo.value.indexOf('@') < 0){
+        alert ("correo incorrecto");
+        return false;
+
+    } else {
+        return true;
+    }
+}
