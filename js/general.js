8bdb20e9f1c5a669df0c2715cd7de64f09b48050
ikerguerre99 <57668500+ikerguerre99@users.noreply.github.com>
2020-10-22 09:52:27 +0200

    Todos los enlaces funcionando

---
+++ js/general.js
@@ -0,0 +1,10 @@
+function paginaPrincipal(){
+    document.location.href = "index.html";
+}
+
+function paginaContacto(){
+    document.location.href = "contacto.html";
+}
+function paginaMaletas(){
+    document.location.href = "maletas.html";
+}
